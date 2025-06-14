7088679ddbe0d845c3a37c20a574e0eadf7e38d1
Farraz Hadyan <farrazhadyan16@gmail.com>
2025-06-14 18:12:07 +0700

    edit sampah sudah benar

--- app/Http/Controllers/SampahController.php
+++ app/Http/Controllers/SampahController.php
@@ -10,33 +10,38 @@ class SampahController extends Controller
     public function index()
     {
         $listsampah = Sampah::all();
-        return view('sampah.index', compact('listsampah'));
+        return view("sampah.index", compact("listsampah"));
     }
-//     public function edit($id)
-// {
-//     $sampah = Sampah::findOrFail($id);
-//     return view('admin.edit', compact('sampah'));
-// }
+    //     public function edit($id)
+    // {
+    //     $sampah = Sampah::findOrFail($id);
+    //     return view('admin.edit', compact('sampah'));
+    // }
 
-public function update(Request $request, $id)
-{
-    $request->validate([
-        'jenis_sampah' => 'required|string',
-        'harga_satuan' => 'required|numeric|min:0',
-        'jumlah' => 'required|numeric|min:0',
-    ]);
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            "jenis_sampah" => "required|string",
+            "harga_satuan" => "required|numeric|min:0",
+            "jumlah" => "required|numeric|min:0",
+        ]);
 
-    $sampah = Sampah::findOrFail($id);
-    $sampah->update($request->only('jenis_sampah', 'harga_satuan', 'jumlah'));
+        $sampah = Sampah::findOrFail($id);
+        $sampah->update(
+            $request->only("jenis_sampah", "harga_satuan", "jumlah")
+        );
 
-    return redirect()->back()->with('success', 'Data sampah berhasil diperbarui!');
-}
-// SampahController.php
-public function destroy($id)
-{
-    $sampah = Sampah::findOrFail($id);
-    $sampah->delete();
-    return redirect()->route('sampah.index')->with('success', 'Data berhasil dihapus.');
-}
-
+        return redirect()
+            ->back()
+            ->with("success", "Data sampah berhasil diperbarui!");
+    }
+    // SampahController.php
+    public function destroy($id)
+    {
+        $sampah = Sampah::findOrFail($id);
+        $sampah->delete();
+        return redirect()
+            ->route("sampah.index")
+            ->with("success", "Data berhasil dihapus.");
+    }
 }
--- app/Models/Sampah.php
+++ app/Models/Sampah.php
@@ -5,15 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
 class Sampah extends Model
 {
     use HasFactory;
 
-    protected $table = 'sampah'; // ← beri tahu nama tabel sebenarnya
+    protected $table = "sampah"; // ← beri tahu nama tabel sebenarnya
 
-    protected $fillable = [
-        'jenis_sampah',
-        'harga_satuan'
-    ];
+    protected $fillable = ["jenis_sampah", "harga_satuan", "jumlah"];
 }
